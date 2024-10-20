7d0c6d58d2c5a9203f75cc43a1c56d2ea4444d2f
braguilera <braaguileraa@gmail.com>
2024-10-19 22:33:57 -0300

    creacion de rutas con su contenido

---
+++ src/rutas/RutaPrincipal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RutaPrincipal = () => {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default RutaPrincipal
---
+++ src/rutas/RutaSecundaria.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RutaSecundaria = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default RutaSecundaria
