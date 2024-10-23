758b51548375ea60c18de37c1afd5080e0b34ec3
braguilera <braaguileraa@gmail.com>
2024-10-23 12:05:28 -0300

    Navegador con estilos listo

--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     return (
     <div>
             <h1>Login</h1>
-            <button onClick={login}>Cerrar sesión</button>
+            <button onClick={login}>Iniciar sesión</button>
     </div>
     )
 }
--- src/componentes/Nav.jsx
+++ src/componentes/Nav.jsx
@@ -1,51 +1,83 @@
-import React, { useContext, useState } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
+import React, { useContext, useState, useEffect } from 'react';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 import Contexto from '../contexto/Contexto';
 
 const Nav = () => {
-    const navegacion=useNavigate();
-    const {deslogearse}=useContext(Contexto);
+    const navegacion = useNavigate();
+    const location = useLocation(); // Hook para obtener la ubicación actual
+    const { deslogearse } = useContext(Contexto);
     const [activo, setActivo] = useState(false);
-    
-    const logout=()=>{
-        deslogearse();
-        navegacion('/login',{replace:true})
-    }
+    const [cambiar, setCambiar] = useState(false);
 
-    const cambiarClase=()=>{
-        setActivo(!activo)
-    }
+    const logout = () => {
+        deslogearse();
+        navegacion('/login', { replace: true });
+    };
+
+    const cambiarClase = () => {
+        setActivo(!activo);
+    };
+
+    useEffect(() => {
+        if (location.pathname === '/inicio') {
+            setActivo(false);
+            setCambiar(false);
+        }
+    }, [location]);
+
+
+    const handleNavLinkClick = () => {
+        setCambiar(true);
+    };
 
     return (
         <>
             <nav className='navegador'>
-                <NavLink to="inicio">Inicio</NavLink>
+                <div className='navegador_contenedor'>
+                    <NavLink to="inicio" className={
+                        ({ isActive }) => (isActive ? "activado" : null)
+                    }>Inicio</NavLink>
 
-                <div className={activo 
-                ? "reclamos_details_activo"
-                : "reclamos_details_desactivado"
-                }
-                onClick={cambiarClase}>
-                    <h2>Reclamos</h2>
-                    <strong>{activo 
-                ? "-"
-                : "+"
-                }</strong>
+                    <div className={(activo) 
+                        ? (cambiar) ? "reclamos_details_activo" : "reclamos_details"
+                        : "reclamos_details_desactivado"
+                    }
+                    onClick={cambiarClase}>
+                        <h2>Reclamos</h2>
+                        <strong>{activo ? "-" : "+"}</strong>
+                    </div>
+
+                    <div className={(activo) 
+                        ? (cambiar) ? "reclamos_summary_activo" : "reclamos_sumary"
+                        : "reclamos_summary_desactivado"
+                    }>
+                        <NavLink
+                            to="verReclamos" 
+                            className={
+                                ({ isActive }) => (isActive ? "activado_secundario" : "reclamo_secundario")
+                            }
+                            onClick={handleNavLinkClick} // Maneja el click
+                        >
+                            Ver reclamos
+                        </NavLink>
+
+                        <NavLink 
+                            to="crearReclamo" 
+                            className={
+                                ({ isActive }) => (isActive ? "activado_secundario" : "reclamo_secundario")
+                            }
+                            onClick={handleNavLinkClick} // Maneja el click
+                        >
+                            Crear reclamo
+                        </NavLink>
+                    </div>
                 </div>
-
-                <div className={activo 
-                ? "reclamos_summary_desactivado"
-                : "reclamos_summary_activo"
-                }>
-                        <NavLink to="verReclamos">Ver reclamos</NavLink>
-                        <NavLink to="crearReclamo">Crear reclamo</NavLink>
+                <div className='navegador_contenedor_botones'>
+                    <button onClick={logout}>Cerrar sesión</button>
                 </div>
-
-                <button onClick={logout}>Cerrar sesión</button>
-
             </nav>
         </>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;
