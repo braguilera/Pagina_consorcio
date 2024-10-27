930d2e33faca4292fb9c6cc9b2c2dee1dae25791
braguilera <braaguileraa@gmail.com>
2024-10-27 18:14:14 -0300

    Paginacion de edificios lista

--- src/componentes/edificios/Edificios.jsx
+++ src/componentes/edificios/Edificios.jsx
@@ -1,24 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { fetchDatos } from '../../datos/fetchDatos';
+import { MagicMotion } from 'react-magic-motion';
 
 const Edificios = () => {
     const [edificios, setEdificios] = useState([]);
     const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false); // Estado de carga
+    const [loading, setLoading] = useState(false);
     const [nuevoEdificio, setNuevoEdificio] = useState({ nombre: '', direccion: '' });
     const [mostrarError, setMostrarError] = useState(false);
+    const [idBusqueda, setIdBusqueda] = useState('');
+    const [edificiosFiltrados, setEdificiosFiltrados] = useState([]);
+    const [paginaActual, setPaginaActual] = useState(1);
+    const edificiosPorPagina = 10;
 
-    // Función para obtener la lista de edificios usando fetchDatos
     const obtenerEdificios = async () => {
-        setLoading(true); // Activar el estado de carga
+        setLoading(true);
         try {
-            const data = await fetchDatos('http://localhost:8080/edificio/todos_edificios');
+            const data = await fetchDatos('http://localhost:8080/edificio/edificios');
             setEdificios(data);
+            setEdificiosFiltrados(data);
         } catch (error) {
             setError(error.message);
+            setMostrarError(true);
+            setTimeout(() => setMostrarError(false), 3000);
         } finally {
-            setLoading(false); // Desactivar el estado de carga
+            setLoading(false);
         }
+    };
+
+    const filtrarEdificios = (event) => {
+        const idEdificio = event.target.value;
+        setIdBusqueda(idEdificio);
+
+        if (idEdificio === '') {
+            setEdificiosFiltrados(edificios);
+        } else {
+            const filtrados = edificios.filter(edificio =>
+                edificio.codigo.toString().startsWith(idEdificio)
+            );
+            setEdificiosFiltrados(filtrados);
+        }
+        setPaginaActual(1);
     };
 
     useEffect(() => {
@@ -55,6 +77,7 @@ const Edificios = () => {
 
             const data = await response.json();
             setEdificios(prevEdificios => [...prevEdificios, data]);
+            setEdificiosFiltrados(prevEdificios => [...prevEdificios, data]);
             setNuevoEdificio({ nombre: '', direccion: '' });
         } catch (error) {
             setError(error.message);
@@ -63,71 +86,147 @@ const Edificios = () => {
         }
     };
 
+    const indiceInicio = (paginaActual - 1) * edificiosPorPagina;
+    const indiceFin = indiceInicio + edificiosPorPagina;
+    const edificiosPaginados = edificiosFiltrados.slice(indiceInicio, indiceFin);
+    const totalPaginas = Math.ceil(edificiosFiltrados.length / edificiosPorPagina);
+
+    const irAPagina = (numeroPagina) => {
+        setPaginaActual(numeroPagina);
+    };
+
     return (
-        <div>
-            <h2>Lista de Edificios</h2>
+        <>
+            <MagicMotion>
+                <section className='edificios'>
+                    <h2>Lista de Edificios</h2>
+                    <main className='edificios_main'>
+                        {loading ? (
+                            <div className='tabla_cargando'>Cargando...</div>
+                        ) : (
+                            
+                            <table className='tabla_container'>
+                                <div>
+                                    <input
+                                        id='idEdificio'
+                                        type='number'
+                                        placeholder='Buscar por ID'
+                                        value={idBusqueda}
+                                        onChange={filtrarEdificios}
+                                    />
+                                    <tbody className='tabla_body'>
+                                        <thead className='tabla_encabezado'>
+                                            <tr>
+                                                <th>Id </th>
+                                                <th>Nombre</th>
+                                                <th>Dirección</th>
 
-            {loading ? (
-                <p>Cargando...</p>
-            ) : (
-                <table className='tabla_container'>
-                    <tbody className='tabla_body'>
-                        
-                            {edificios.map(edificio => (
-                                <tr className='tabla_objeto' key={edificio.codigo}> 
-                                    <td>{edificio.codigo} </td>
-                                    <td>{edificio.nombre}  </td>
-                                    <td>{edificio.direccion} </td>
-                                </tr>
-                            ))}
-                        
-                    </tbody>
-                </table>
-            )}
+                                            </tr>
+                                        </thead>
+                                        {edificiosPaginados.length > 0 ? (
+                                            edificiosPaginados.map(edificio => (
+                                                <tr className='tabla_objeto' key={edificio.codigo}>
+                                                    <td>{edificio.codigo}</td>
+                                                    <td>{edificio.nombre}</td>
+                                                    <td>{edificio.direccion}</td>
+                                                </tr>
+                                            ))
+                                        ) : (
+                                            <tr>
+                                                <td colSpan="3">No se encontró ningún edificio.</td>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                </div>
+                                <div className='paginacion'>
+                                    <button 
+                                        onClick={() => irAPagina(paginaActual - 1)}  
+                                        style={{
+                                            opacity: paginaActual > 1 ? 1 : 0,
+                                            pointerEvents: paginaActual > 1 ? 'auto' : 'none',
+                                            backgroundColor: 'transparent'
+                                        }}
+                                    >
+                                        ⬅️
+                                    </button>
 
-            <h3>Agregar Nuevo Edificio</h3>
-            <form onSubmit={manejarSubmit}>
-                <label>
-                    Nombre:
-                    <input
-                        type="text"
-                        name="nombre"
-                        value={nuevoEdificio.nombre}
-                        onChange={manejarCambio}
-                        required
-                    />
-                </label>
-                <br />
-                <label>
-                    Dirección:
-                    <input
-                        type="text"
-                        name="direccion"
-                        value={nuevoEdificio.direccion}
-                        onChange={manejarCambio}
-                        required
-                    />
-                </label>
-                <br />
-                <button type="submit">Agregar Edificio</button>
-            </form>
+                                    {Array.from({ length: totalPaginas }).map((_, index) => (
+                                        <button
+                                            key={index}
+                                            onClick={() => irAPagina(index + 1)}
+                                            style={{
+                                                backgroundColor: paginaActual === index + 1 ? '#4b83c1' : undefined,
+                                                color: paginaActual === index + 1 ? '#f4f5f5' : undefined
+                                            }}
+                                        >
+                                            {index + 1}
+                                        </button>
+                                    ))}
 
-            {/* Mostrar el pop-up del error */}
-            {mostrarError && (
-                <div style={{
-                    position: 'fixed',
-                    bottom: '20px',
-                    right: '20px',
-                    backgroundColor: 'red',
-                    color: 'white',
-                    padding: '10px',
-                    borderRadius: '5px',
-                    zIndex: '1000'
-                }}>
-                    Error: {error}
-                </div>
-            )}
-        </div>
+                                    <button 
+                                        onClick={() => irAPagina(paginaActual + 1)}  
+                                        style={{
+                                            opacity: paginaActual < totalPaginas ? 1 : 0,
+                                            pointerEvents: paginaActual < totalPaginas ? 'auto' : 'none',
+                                            backgroundColor: 'transparent'
+                                        }}
+                                    >
+                                        ➡️
+                                    </button>
+                                </div>
+                            </table>
+
+
+                            
+                        )}
+
+                        <aside>
+                            <h3>Agregar Nuevo Edificio</h3>
+                            <form onSubmit={manejarSubmit}>
+                                <label>
+                                    Nombre:
+                                    <input
+                                        type="text"
+                                        name="nombre"
+                                        value={nuevoEdificio.nombre}
+                                        onChange={manejarCambio}
+                                        required
+                                    />
+                                </label>
+                                <br />
+                                <label>
+                                    Dirección:
+                                    <input
+                                        type="text"
+                                        name="direccion"
+                                        value={nuevoEdificio.direccion}
+                                        onChange={manejarCambio}
+                                        required
+                                    />
+                                </label>
+                                <br />
+                                <button type="submit">Agregar Edificio</button>
+                            </form>
+                        </aside>
+                    </main>
+
+                    {mostrarError && (
+                        <div style={{
+                            position: 'fixed',
+                            bottom: '20px',
+                            right: '20px',
+                            backgroundColor: 'red',
+                            color: 'white',
+                            padding: '10px',
+                            borderRadius: '5px',
+                            zIndex: '1000'
+                        }}>
+                            Error: {error}
+                        </div>
+                    )}
+                </section>
+            </MagicMotion>
+        </>
     );
 };
 
