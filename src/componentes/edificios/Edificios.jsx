32797d75c31d32ae8536b750bd35e34428345ac9
braguilera <braaguileraa@gmail.com>
2024-10-27 19:15:14 -0300

    Estilos en edificios listo

--- src/componentes/edificios/Edificios.jsx
+++ src/componentes/edificios/Edificios.jsx
@@ -97,97 +97,99 @@ const Edificios = () => {
 
     return (
         <>
-            <MagicMotion>
+
                 <section className='edificios'>
                     <h2>Lista de Edificios</h2>
                     <main className='edificios_main'>
-                        {loading ? (
-                            <div className='tabla_cargando'>Cargando...</div>
-                        ) : (
-                            
-                            <table className='tabla_container'>
-                                <div>
-                                    <input
-                                        id='idEdificio'
-                                        type='number'
-                                        placeholder='Buscar por ID'
-                                        value={idBusqueda}
-                                        onChange={filtrarEdificios}
-                                    />
-                                    <tbody className='tabla_body'>
-                                        <thead className='tabla_encabezado'>
-                                            <tr>
-                                                <th>Id </th>
-                                                <th>Nombre</th>
-                                                <th>Dirección</th>
+                        <MagicMotion>
+                            {loading ? (
+                                <div className='tabla_cargando'>Cargando...</div>
+                            ) : (
+                                
+                                <table className='tabla_container'>
+                                    <div className='tabla_container_items'>
+                                        <input
+                                            id='idEdificio'
+                                            type='number'
+                                            placeholder='Buscar por ID'
+                                            value={idBusqueda}
+                                            onChange={filtrarEdificios}
+                                        />
+                                        <tbody className='tabla_body'>
+                                            <thead className='tabla_encabezado'>
+                                                <tr>
+                                                    <th>Id </th>
+                                                    <th>Nombre</th>
+                                                    <th>Dirección</th>
 
-                                            </tr>
-                                        </thead>
-                                        {edificiosPaginados.length > 0 ? (
-                                            edificiosPaginados.map(edificio => (
-                                                <tr className='tabla_objeto' key={edificio.codigo}>
-                                                    <td>{edificio.codigo}</td>
-                                                    <td>{edificio.nombre}</td>
-                                                    <td>{edificio.direccion}</td>
                                                 </tr>
-                                            ))
-                                        ) : (
-                                            <tr>
-                                                <td colSpan="3">No se encontró ningún edificio.</td>
-                                            </tr>
-                                        )}
-                                    </tbody>
-                                </div>
-                                <div className='paginacion'>
-                                    <button 
-                                        onClick={() => irAPagina(paginaActual - 1)}  
-                                        style={{
-                                            opacity: paginaActual > 1 ? 1 : 0,
-                                            pointerEvents: paginaActual > 1 ? 'auto' : 'none',
-                                            backgroundColor: 'transparent'
-                                        }}
-                                    >
-                                        ⬅️
-                                    </button>
-
-                                    {Array.from({ length: totalPaginas }).map((_, index) => (
-                                        <button
-                                            key={index}
-                                            onClick={() => irAPagina(index + 1)}
+                                            </thead>
+                                            {edificiosPaginados.length > 0 ? (
+                                                edificiosPaginados.map(edificio => (
+                                                    <tr className='tabla_objeto' key={edificio.codigo}>
+                                                        <td>{edificio.codigo}</td>
+                                                        <td>{edificio.nombre}</td>
+                                                        <td>{edificio.direccion}</td>
+                                                    </tr>
+                                                ))
+                                            ) : (
+                                                <tr>
+                                                    <td colSpan="3">No se encontró ningún edificio.</td>
+                                                </tr>
+                                            )}
+                                        </tbody>
+                                    </div>
+                                    <div className='paginacion'>
+                                        <button 
+                                            onClick={() => irAPagina(paginaActual - 1)}  
                                             style={{
-                                                backgroundColor: paginaActual === index + 1 ? '#4b83c1' : undefined,
-                                                color: paginaActual === index + 1 ? '#f4f5f5' : undefined
+                                                opacity: paginaActual > 1 ? 1 : 0,
+                                                pointerEvents: paginaActual > 1 ? 'auto' : 'none',
+                                                backgroundColor: 'transparent'
                                             }}
                                         >
-                                            {index + 1}
+                                            ⬅️
                                         </button>
-                                    ))}
 
-                                    <button 
-                                        onClick={() => irAPagina(paginaActual + 1)}  
-                                        style={{
-                                            opacity: paginaActual < totalPaginas ? 1 : 0,
-                                            pointerEvents: paginaActual < totalPaginas ? 'auto' : 'none',
-                                            backgroundColor: 'transparent'
-                                        }}
-                                    >
-                                        ➡️
-                                    </button>
-                                </div>
-                            </table>
+                                        {Array.from({ length: totalPaginas }).map((_, index) => (
+                                            <button
+                                                key={index}
+                                                onClick={() => irAPagina(index + 1)}
+                                                style={{
+                                                    backgroundColor: paginaActual === index + 1 ? '#4b83c1' : undefined,
+                                                    color: paginaActual === index + 1 ? '#f4f5f5' : undefined
+                                                }}
+                                            >
+                                                {index + 1}
+                                            </button>
+                                        ))}
+
+                                        <button 
+                                            onClick={() => irAPagina(paginaActual + 1)}  
+                                            style={{
+                                                opacity: paginaActual < totalPaginas ? 1 : 0,
+                                                pointerEvents: paginaActual < totalPaginas ? 'auto' : 'none',
+                                                backgroundColor: 'transparent'
+                                            }}
+                                        >
+                                            ➡️
+                                        </button>
+                                    </div>
+                                </table>
 
 
-                            
-                        )}
-
-                        <aside>
+                                
+                            )}
+                        </MagicMotion>
+                        <aside className='agregar_edificio_container'>
                             <h3>Agregar Nuevo Edificio</h3>
-                            <form onSubmit={manejarSubmit}>
+                            <form onSubmit={manejarSubmit} className='agregar_edificio_form'>
                                 <label>
                                     Nombre:
                                     <input
                                         type="text"
                                         name="nombre"
+                                        placeholder='Ingresar un nombre'
                                         value={nuevoEdificio.nombre}
                                         onChange={manejarCambio}
                                         required
@@ -199,6 +201,7 @@ const Edificios = () => {
                                     <input
                                         type="text"
                                         name="direccion"
+                                        placeholder='Ingresar una dirección'
                                         value={nuevoEdificio.direccion}
                                         onChange={manejarCambio}
                                         required
@@ -225,7 +228,7 @@ const Edificios = () => {
                         </div>
                     )}
                 </section>
-            </MagicMotion>
+            
         </>
     );
 };
