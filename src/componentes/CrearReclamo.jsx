0e65c4dc67cd7c15519769cb23eff157fa46ddd0
braguilera <braaguileraa@gmail.com>
2024-11-10 22:27:48 -0300

    Estilos e inputs en crear reclamo

--- src/componentes/CrearReclamo.jsx
+++ src/componentes/CrearReclamo.jsx
@@ -12,38 +12,43 @@ const CrearReclamo = () => {
             <main className='crearReclamo_main'>
                 <aside className='crearReclamo_imagen'></aside>
                 <article className='crearReclamo_container'>
-                    <h2> Detallanos tu solicitud </h2>
-                    <header className='crearReclamo_container_header'>
-                        <article className='crearReclamo_inputs'>
-                            <h3>¿En qué zona es el reclamo?</h3>
-                            
-                            <div className='crearReclamo_inputs_radio'>
-                                <input type='radio'/>
-                                <p>Zona común</p>
-                                <input type='radio'/>
-                                <p>Vivienda</p>
-                            </div>
+                <h2> Detallanos tu solicitud </h2>
+                    
+                    <div className='crearReclamo_grid'>
+                        <header className='crearReclamo_container_header'>
+                            <article className='crearReclamo_inputs'>
+                                <h3>¿En qué zona es el reclamo?</h3>
+                                <div className='crearReclamo_inputs_radio'>
+                                    
+                                    <input type='radio'/>
+                                    <p>Zona común</p>
+                                    <input type='radio'/>
+                                    <p>Vivienda</p>
+                                </div>
 
 
-                            <h3>Vivienda</h3>
-                            <input type='text'/>
-                            <h3>Tema</h3> 
-                            <select>
-                                <option>Plomería</option>
-                                <option>Electricidad</option>
-                                <option>Otro...</option>
-                            </select>
-                        </article>
-            
-                        <article className='crearReclamo_descripcion'>
-                            <h3>Descripción (Máximo 200 carácteres)</h3>
-                            <input type='textarea'/>
-                        </article>
-                    </header>
+                                <h3>Vivienda</h3>
+                                <input type='text'/>
+                                <h3>Tema</h3> 
+                                <select>
+                                    <option>Plomería</option>
+                                    <option>Electricidad</option>
+                                    <option>Otro...</option>
+                                </select>
+                            </article>
+                
+                            <article className='crearReclamo_descripcion'>
+                                <h3>Descripción (Máximo 200 carácteres)</h3>
+                                <textarea  maxLength={200}/>
+                            </article>
+                        </header>
 
-                    <aside className='crearReclamo_adjuntarImagenes'>
-                        <h3>Adjuntar imagenes (Máximo 3 imágenes)</h3>
-                    </aside>
+                        <aside className='crearReclamo_adjuntarImagenes'>
+                            <h3>Adjuntar imagenes (Máximo 3 imágenes)</h3>
+                        </aside>
+                    </div>
+
+                    
 
                 </article>
             </main>
